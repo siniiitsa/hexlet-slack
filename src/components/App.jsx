dc3590ea6ed84952478dde381161c8f38051f64d
Sergey <siniiitsa@gmail.com>
2020-10-19 19:47:38 +0300

    change some names

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,13 @@ import Channels from './Channels';
 import Messages from './Messages';
 
 const App = ({ channels, messages, currentChannelId }) => {
-  const channelsData = { channels, currentChannelId };
-  const messagesData = { messages, currentChannelId };
+  const channelsProps = { channels, currentChannelId };
+  const messagesProps = { messages, currentChannelId };
 
   return (
     <div className="row h-100 pb-3">
-      <Channels {...channelsData} />
-      <Messages {...messagesData} />
+      <Channels {...channelsProps} />
+      <Messages {...messagesProps} />
     </div>
   );
 };
