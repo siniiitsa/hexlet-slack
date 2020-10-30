30e614b99b0a27d8417dda3adbf9ab528b281cc8
Sergey <siniiitsa@gmail.com>
2020-10-31 00:41:44 +0200

    refactor channels code

---
+++ src/components/ChannelButton.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Button, Dropdown, ButtonGroup } from 'react-bootstrap';
+
+const renderButton = ({ name, variant, onClick }) => (
+  <Button
+    onClick={onClick}
+    className="btn-block mb-2 text-left shadow-none"
+    variant={variant}>
+    {name}
+  </Button>
+);
+
+const renderButtonWithMenu = ({
+  name,
+  variant,
+  onClick,
+  onChannelRename,
+  onChannelRemove,
+}) => (
+  <Dropdown as={ButtonGroup} className="btn-block mb-2">
+    <Button
+      onClick={onClick}
+      className="text-left shadow-none"
+      variant={variant}>
+      {name}
+    </Button>
+
+    <Dropdown.Toggle
+      split
+      className="flex-grow-0 shadow-none"
+      variant={variant}
+      id="dropdown-split-basic"
+    />
+
+    <Dropdown.Menu>
+      <Dropdown.Item onClick={onChannelRename}>Rename</Dropdown.Item>
+      <Dropdown.Item onClick={onChannelRemove}>Remove</Dropdown.Item>
+    </Dropdown.Menu>
+  </Dropdown>
+);
+
+const ChannelButton = (props) => {
+  const render = props.removable ? renderButtonWithMenu : renderButton;
+  return render(props);
+};
+
+export default ChannelButton;
--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -1,60 +1,9 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  Col,
-  NavItem,
-  Button,
-  SplitButton,
-  Dropdown,
-  ButtonGroup,
-} from 'react-bootstrap';
+import { Col, NavItem, Button, Dropdown, ButtonGroup } from 'react-bootstrap';
 import getModal from './modals';
+import ChannelButton from './ChannelButton';
 import { changeCurrentChannel, selectAllChannles } from '../store/channels';
-
-const renderButton = (title, variant, handleClick) => (
-  <Button
-    onClick={handleClick}
-    className="btn-block mb-2 text-left shadow-none"
-    variant={variant}>
-    {title}
-  </Button>
-);
-
-const renderSplitButton = (title, variant, handleClick) => (
-  <Dropdown as={ButtonGroup} className="btn-block mb-2">
-    <Button
-      onClick={handleClick}
-      className="text-left shadow-none"
-      variant={variant}>
-      {title}
-    </Button>
-
-    <Dropdown.Toggle
-      split
-      variant={variant}
-      id="dropdown-split-basic"
-      className="flex-grow-0 shadow-none"
-    />
-
-    <Dropdown.Menu>
-      <Dropdown.Item>Rename</Dropdown.Item>
-      <Dropdown.Item>Remove</Dropdown.Item>
-    </Dropdown.Menu>
-  </Dropdown>
-);
-
-const renderChannels = (channels, currentChannelId, handleChangeChannel) => {
-  if (channels.length === 0) {
-    return null;
-  }
-
-  return channels.map(({ id, name, removable }) => {
-    const handleClick = handleChangeChannel(id);
-    const variant = id === currentChannelId ? 'primary' : 'light';
-    const render = removable ? renderSplitButton : renderButton;
-    return <NavItem key={id}>{render(name, variant, handleClick)}</NavItem>;
-  });
-};
 
 const renderModal = (modalInfo, hideModal) => {
   if (!modalInfo.type) return null;
@@ -72,9 +21,10 @@ const Channels = () => {
   );
 
   const hideModal = () => setModalInfo({ type: null, item: null });
-  const showModal = (type, item = null) => () => setModalInfo({ type, item });
+  const createShowModalHandler = (type, item = null) => () =>
+    setModalInfo({ type, item });
 
-  const handleChangeChannel = (channelId) => () =>
+  const createChangeChannelHandler = (channelId) => () =>
     dispatch(changeCurrentChannel({ channelId }));
 
   return (
@@ -82,14 +32,25 @@ const Channels = () => {
       <div className="d-flex my-2">
         <span>Channels</span>
         <Button
-          onClick={showModal('addChannel')}
           className="ml-auto p-0"
+          onClick={createShowModalHandler('addChannel')}
           variant="link">
           +
         </Button>
       </div>
       <ul className="nav flex-column nav-pills nav-fill">
-        {renderChannels(channels, currentChannelId, handleChangeChannel)}
+        {channels.map(({ id, name, removable }) => (
+          <NavItem key={id}>
+            <ChannelButton
+              name={name}
+              removable={removable}
+              variant={id === currentChannelId ? 'primary' : 'light'}
+              onClick={createChangeChannelHandler(id)}
+              onChannelRename={createShowModalHandler('addChannel')} // TODO: change modal type
+              onChannelRemove={createShowModalHandler('addChannel')} // TODO: change modal type
+            />
+          </NavItem>
+        ))}
       </ul>
       {renderModal(modalInfo, hideModal)}
     </Col>
